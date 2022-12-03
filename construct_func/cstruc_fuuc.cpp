bec91db6069e0840c59fa91f77604c53d43fcf1d
kongfanyin <103018828+kongfanyin@users.noreply.github.com>
2022-12-03 20:27:54 +0800

    类-构造函数

---
+++ construct_func/cstruc_fuuc.cpp
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include<string>
+#include<fstream>
+#include<iostream>
+using namespace std;
+class cellphone
+{
+private:
+	string name;
+	char num[11];
+public:
+	cellphone(string name)
+	{
+		
+
+	}
+
+};
+class Human
+{
+private:
+	string name;
+	cellphone p1;
+	
+protected:
+
+
+
+public:
+	
+
+
+};
+int main(char argc, char** argv)
+{
+
+
+
+
+	return 0;
+}
+	
